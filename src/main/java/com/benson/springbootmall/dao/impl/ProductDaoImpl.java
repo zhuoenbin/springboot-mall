9f3be2394c843387a991946c0abba0cb017ce0d4
zhuoenbin <boy820408@gmail.com>
2024-03-03 14:57:04 +0800

    商品功能 - 實作刪除商品

--- src/main/java/com/benson/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/benson/springbootmall/dao/impl/ProductDaoImpl.java
@@ -83,4 +83,12 @@ public class ProductDaoImpl implements ProductDao {
 
         namedParameterJdbcTemplate.update(sql,map);
     }
+
+    @Override
+    public void deleteProductById(Integer productId) {
+        String sql ="delete from product where product_id = :productId";
+        Map<String, Object> map = new HashMap<>();
+        map.put("productId",productId);
+        namedParameterJdbcTemplate.update(sql,map);
+    }
 }
